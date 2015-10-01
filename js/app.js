fff290d8cee7c762ef9cb2fa6730be51b1c11e77
Javier Sanchez <reivaj0705@gmail.com>
2015-10-01 14:57:28 -0500

    Add functionality to add locations

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@
 var ViewModel = function(){
     var self = this;
     self.locations = ko.observableArray(JSON.parse(localStorage.locations));
+    self.locationLabel = ko.observable('');
     self.showAddLocationForm = ko.observable(false);
 
     self.editLocation = function(){
@@ -18,7 +19,11 @@ var ViewModel = function(){
         self.updateLocations();
     };
     self.addLocation = function(){
-        console.log('addLocation');
+        self.locations.push({
+            location: self.locationLabel()
+        });
+        self.updateLocations();
+        self.updateShowAddLocationForm();
     };
 
     self.updateLocations = function(){
@@ -26,6 +31,7 @@ var ViewModel = function(){
     };
 
     self.updateShowAddLocationForm = function(){
+        self.locationLabel('');
         self.showAddLocationForm(!self.showAddLocationForm());
     };
 
