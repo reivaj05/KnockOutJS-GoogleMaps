f56341ca314cc15defc719f27a1ac5ffa4380367
Javier Sanchez <reivaj0705@gmail.com>
2015-10-03 15:49:39 -0500

    Create instagram class

---
+++ js/instagram.js
@@ -0,0 +1,12 @@
+var Instagram = function(){
+
+};
+
+Instagram.prototype.searchLocation = function(place, marker){
+    console.log(place);
+    console.log(marker);
+};
+
+Instagram.prototype.getResults = function(){
+
+};
